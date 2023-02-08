9b0f4ef2488280f88d657ea05667f8b3024a91df
Edsoncrf <mocosinn@gmail.com>
2023-02-08 19:44:14 -0300

    correção e retirando teste

--- pessoas/src/main/java/Edsoncrf/domain/repository/Pessoas.java
+++ pessoas/src/main/java/Edsoncrf/domain/repository/Pessoas.java
@@ -10,16 +10,16 @@ import java.util.List;
 
 
 public interface Pessoas extends JpaRepository<Pessoa, Integer> {
-
-    @Query(value = "SELECT * FROM pessoa c WHERE c.nome LIKE '%:nome%'", nativeQuery = true)
-    List<Pessoa> encontraPorNome(@Param("nome") String nome);
-
-    @Query("DELETE FROM Pessoa c WHERE c.nome = :nome")
-    @Modifying //para update e delete usasse essa anotação
-    void deleteByNome(String nome);
-
-    boolean existsByNome(String nome);
-    @Query("SELECT c FROM Pessoa c LEFT JOIN FETCH c.enderecos WHERE c.id = :id")
-    Pessoa findPessoaFetchEnderecos(@Param("id") Integer id);
+//
+//    @Query(value = "SELECT * FROM pessoa c WHERE c.nome LIKE '%:nome%'", nativeQuery = true)
+//    List<Pessoa> encontraPorNome(@Param("nome") String nome);
+//
+//    @Query("DELETE FROM Pessoa c WHERE c.nome = :nome")
+//    @Modifying //para update e delete usasse essa anotação
+//    void deleteByNome(String nome);
+//
+//    boolean existsByNome(String nome);
+//    @Query("SELECT c FROM Pessoa c LEFT JOIN FETCH c.enderecos WHERE c.id = :id")
+//    Pessoa findPessoaFetchEnderecos(@Param("id") Integer id);
 
 }
